b20f6eca03b452498a5093d1678b00601589f3a0
Malta Micael <mmicael@gmail.com>
2021-06-10 21:50:10 -0400

    CPureBlockHeader 1

--- test/functional/interface_rest.py
+++ test/functional/interface_rest.py
@@ -207,6 +207,7 @@ class RESTTest (BitcoinTestFramework):
 
         self.nodes[0].generate(1)  # generate block to not affect upcoming tests
         self.sync_all()
+        bb_hash = self.nodes[0].getbestblockhash()
 
         self.log.info("Test the /block, /blockhashbyheight and /headers URIs")
         bb_hash = self.nodes[0].getbestblockhash()
@@ -228,9 +229,10 @@ class RESTTest (BitcoinTestFramework):
 
         # Compare with block header
         response_header = self.test_rest_request("/headers/1/{}".format(bb_hash), req_type=ReqType.BIN, ret_type=RetType.OBJ)
-        assert_equal(int(response_header.getheader('content-length')), BLOCK_HEADER_SIZE)
+        headerLen = int(response_header.getheader('content-length'))
+        assert_greater_than(headerLen, BLOCK_HEADER_SIZE)
         response_header_bytes = response_header.read()
-        assert_equal(response_bytes[:BLOCK_HEADER_SIZE], response_header_bytes)
+        assert_equal(response_bytes[:headerLen], response_header_bytes)
 
         # Check block hex format
         response_hex = self.test_rest_request("/block/{}".format(bb_hash), req_type=ReqType.HEX, ret_type=RetType.OBJ)
@@ -241,8 +243,9 @@ class RESTTest (BitcoinTestFramework):
         # Compare with hex block header
         response_header_hex = self.test_rest_request("/headers/1/{}".format(bb_hash), req_type=ReqType.HEX, ret_type=RetType.OBJ)
         assert_greater_than(int(response_header_hex.getheader('content-length')), BLOCK_HEADER_SIZE*2)
-        response_header_hex_bytes = response_header_hex.read(BLOCK_HEADER_SIZE*2)
-        assert_equal(binascii.hexlify(response_bytes[:BLOCK_HEADER_SIZE]), response_header_hex_bytes)
+        response_header_hex_bytes = response_header_hex.read().strip()
+        headerLen = len(response_header_hex_bytes) // 2
+        assert_equal(binascii.hexlify(response_bytes[:headerLen]), response_header_hex_bytes)
 
         # Check json format
         block_json_obj = self.test_rest_request("/block/{}".format(bb_hash))
