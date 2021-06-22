ee0bfeff74e8cdcf3a97dfaa3e70a8f20d56755c
Malta Micael <mmicael@gmail.com>
2021-06-22 16:58:57 -0400

    [CI] MACOS

--- depends/packages/native_cctools.mk
+++ depends/packages/native_cctools.mk
@@ -49,8 +49,8 @@ endef
 define $(package)_preprocess_cmds
   CC=$($(package)_cc) CXX=$($(package)_cxx) INSTALLPREFIX=$($(package)_extract_dir) ./libtapi/build.sh && \
   CC=$($(package)_cc) CXX=$($(package)_cxx) INSTALLPREFIX=$($(package)_extract_dir) ./libtapi/install.sh && \
-  sed -i.old "/define HAVE_PTHREADS/d" $($(package)_build_subdir)/ld64/src/ld/InputFiles.h
-  $($(package)_download_path)/tools/fix_unistd_issue.sh
+  sed -i.old "/define HAVE_PTHREADS/d" $($(package)_build_subdir)/ld64/src/ld/InputFiles.h && \
+  $($(package)_build_subdir)/tools/fix_unistd_issue.sh
 endef
 
 define $(package)_config_cmds
